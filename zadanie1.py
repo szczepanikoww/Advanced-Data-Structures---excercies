fb5275656d91bd73845b57d0d3e00e31dce6e76c
szczepanikoww <166534939+szczepanikoww@users.noreply.github.com>
2025-05-24 19:25:17 +0200

    dodano zadanie1.py

---
+++ zadanie1.py
@@ -0,0 +1,13 @@
+def odwroc_napis(napis):
+    stos = []
+    for znak in napis:
+        stos.append(znak)
+    odwrocony = ''
+    pass
+
+print(odwroc_napis("kajak"))
+print(odwroc_napis("abcd"))
+
+# Oczekiwane wyniki:
+# kajak
+# dcba
