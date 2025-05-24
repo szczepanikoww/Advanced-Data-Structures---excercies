b5831beec42af18999e9e5b5db9dd77ee1e4bd41
szczepanikoww <166534939+szczepanikoww@users.noreply.github.com>
2025-05-24 19:34:00 +0200

    dodano zadanie2.py

---
+++ zadanie2.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def symuluj_drukowanie(kolejka):
+    # WRITE YOUR CODE HERE
+    pass
+
+drukarka = deque(["plik1.pdf", "plik2.pdf", "plik3.pdf"])
+symuluj_drukowanie(drukarka)
+
+# Oczekiwany wynik:
+# plik1.pdf
+# plik2.pdf
+# plik3.pdf
