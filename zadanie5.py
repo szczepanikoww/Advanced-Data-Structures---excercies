36d7a6367a1f890a883acd35c207926ab701ac71
szczepanikoww <166534939+szczepanikoww@users.noreply.github.com>
2025-05-26 16:20:16 +0200

    dodano zadanie5.py

---
+++ zadanie5.py
@@ -0,0 +1,18 @@
+def wypisz_stopnie(graf):
+    # WRITE YOUR CODE HERE
+    pass
+
+graf = [
+    [0, 1, 0, 1],
+    [1, 0, 1, 1],
+    [0, 1, 0, 1],
+    [1, 1, 1, 0]
+]
+
+wypisz_stopnie(graf)
+
+# Oczekiwany wynik:
+# Wierzchołek 0: 2 sąsiadów
+# Wierzchołek 1: 3 sąsiadów
+# Wierzchołek 2: 2 sąsiadów
+# Wierzchołek 3: 3 sąsiadów
